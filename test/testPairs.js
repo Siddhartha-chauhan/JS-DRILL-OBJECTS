fb0e71b104caafb8ba32165aa229f49d3b240144
Siddharthachauhan <siddharthachauhan.connect@gmail.com>
2025-12-04 18:38:08 +0530

    implement pairs function and add test

---
+++ test/testPairs.js
@@ -0,0 +1,6 @@
+import { pairs } from '../pairs.js';
+
+const testObject = { name: 'Bruce Wayne', age: 36, location: 'Gotham' };
+
+console.log(pairs(testObject));
+// Expected output: [['name','Bruce Wayne'], ['age',36], ['location','Gotham']]
