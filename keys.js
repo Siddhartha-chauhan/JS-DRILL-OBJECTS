4a25cb07ae873b9040b3f9b139ab4ea99b445149
Siddharthachauhan <siddharthachauhan.connect@gmail.com>
2025-12-04 18:36:07 +0530

    implement keys function and add test

---
+++ keys.js
@@ -0,0 +1,7 @@
+function keys(obj) {
+    return Object.keys(obj);
+
+    // Return the keys as strings in an array.
+    // Based on http://underscorejs.org/#keys
+}
+export { keys };
---
+++ test/testKeys.js
@@ -0,0 +1,5 @@
+import { keys } from '../keys.js';
+
+const testObject = { name: 'Bruce Wayne', age: 36, location: 'Gotham' };
+
+console.log(keys(testObject)); 
